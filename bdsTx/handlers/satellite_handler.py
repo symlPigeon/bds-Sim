d5e04bd6288d7e4b33501992cd7459f068cf51fe
symlPigeon <2163953074@qq.com>
2022-11-23 16:08:09 +0800

    some update.

--- bdsTx/handlers/satellite_handler.py
+++ bdsTx/handlers/satellite_handler.py
@@ -1,16 +1,17 @@
-'''
+"""
 Author: symlpigeon
 Date: 2022-11-22 11:44:41
-LastEditTime: 2022-11-22 19:50:55
+LastEditTime: 2022-11-23 10:28:45
 LastEditors: symlpigeon
 Description: satellite handler
 FilePath: /bds-Sim/bdsTx/handlers/satellite_handler.py
-'''
+"""
 
+import logging
 from abc import abstractmethod
 from typing import TYPE_CHECKING, List
 
-from base_object import baseSourceHandler
+from base_object import baseProcessorHandler, baseSourceHandler
 
 from bdsTx.frame.b1c import b1c_frame
 
@@ -19,28 +20,53 @@ if TYPE_CHECKING:
 
 
 class satelliteHandler(baseSourceHandler):
-    def __init__(self, prn: int, manager: satelliteQueueHandler, next_handler: baseSourceHandler):
+    def __init__(
+        self,
+        prn: int,
+        manager: satelliteQueueHandler,
+        next_handler: baseProcessorHandler,
+    ):
         self._ranging_code = manager.get_ranging_code(prn)
         self._ephemeris = manager.get_ephemeris(prn)
         self._ldpc_mat_1 = manager.get_ldpc_mat_1()
         self._ldpc_mat_2 = manager.get_ldpc_mat_2()
         self._next_handler = next_handler
-    
+        self._manager = manager
+
     @abstractmethod
     def _produce(self):
         pass
-    
+
     @abstractmethod
     def trigger(self):
         pass
-        
-        
+
+
 class b1cSatelliteHandler(satelliteHandler):
-    def __init__(self, prn: int, manager: satelliteQueueHandler, next_handler: baseSourceHandler, frame_order: List[int]):
+    def __init__(
+        self,
+        prn: int,
+        manager: satelliteQueueHandler,
+        next_handler: baseProcessorHandler,
+        frame_order: List[int],
+    ):
         super().__init__(prn, manager, next_handler)
         self._iono_corr = manager.get_iono_corr("bdgim")
         self._frame_order = frame_order
-        self._frame_creater = b1c_frame.b1cFrame(prn, self._ephemeris, self._iono_corr["alpha"], self._frame_order, self._ldpc_mat_1, self._ldpc_mat_2)
-        
+        self._frame_creater = b1c_frame.b1cFrame(
+            prn,
+            self._ephemeris,
+            self._iono_corr["alpha"],
+            self._frame_order,
+            self._ldpc_mat_1,
+            self._ldpc_mat_2,
+        )
+
     def _produce(self):
-        pass
+        self._next_handler.add(
+            self._frame_creater.make_frame(self._manager.get_curr_time())
+        )
+        
+    def trigger(self):
+        logging.info(f"{self} - b1cSatelliteHandler trigger")
+        self._produce()
