a42a209cc02ac56fc16beaeb958cafb03108322b
symlPigeon <2163953074@qq.com>
2022-12-27 18:03:34 +0800

    LFSR generator.

---
+++ bdsTx/coding/lfsr.py
@@ -0,0 +1,51 @@
+'''
+Author: symlpigeon
+Date: 2022-12-27 17:30:27
+LastEditTime: 2022-12-27 17:59:15
+LastEditors: symlpigeon
+Description: Linear Feedback Shift Register
+FilePath: /bds-Sim/bdsTx/coding/lfsr.py
+'''
+
+import numpy as np
+
+
+def _uint32_swar(x: np.uint32) -> np.uint32:
+    """ unsigned int32 variable presicion SWAR algorithm
+
+    Args:
+        x (int): input number
+
+    Returns:
+        int: Hamming weight of input number
+    """
+    x = np.uint32((x & 0x55555555) + ((x >> 1) & 0x55555555))
+    x = np.uint32((x & 0x33333333) + ((x >> 2) & 0x33333333))
+    x = np.uint32((x & 0x0F0F0F0F) + ((x >> 4) & 0x0F0F0F0F))
+    return x
+
+
+class LFSR:
+    def __init__(self, l: int, g: int, initial_phase: int):
+        self._l = l
+        self._g = g
+        self._reg = initial_phase
+    
+    def get(self):
+        # The output bit is the LSB of the register
+        out_bit = self._reg & 0x1
+        fb_status = self._reg & self._g
+        fb_val = 0
+        while fb_status > 0:
+            fb_val += _uint32_swar(np.uint32(fb_status & 0xffffffff))
+            fb_status >>= 32
+        self._reg >>= 1
+        self._reg |= (fb_val & 0x1) << (self._l - 1)
+        return out_bit
+        
+        
+if __name__ == "__main__":
+    lfsr = LFSR(10, 0b1011100001, 0b0101010101)
+    for i in range(30):
+        print(lfsr.get(), end='')
+    
