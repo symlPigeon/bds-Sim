B1I frame. Fix a subframe field overflow problem in eph params.

bdsTx/frame/b1c/b1c_subframe2.py
@@ -28,11 +28,11 @@ def make_ephemeris1(ephemeris: dict) -> str:
     ans += data2bincomplement(ephemeris["sat_type"], 2, 1)
     ans += data2bincomplement(ephemeris["deltaA"], 26, pow(2, -9))
     ans += data2bincomplement(ephemeris["A_DOT"], 25, pow(2, -21))
-    ans += data2bincomplement(ephemeris["delta_n0"], 17, pow(2, -44))
-    ans += data2bincomplement(ephemeris["delta_n0_dot"], 23, pow(2, -57))
-    ans += data2bincomplement(ephemeris["M0"], 33, pow(2, -32))
+    ans += data2bincomplement(ephemeris["delta_n0"], 17, pow(2, -44) * np.pi)
+    ans += data2bincomplement(ephemeris["delta_n0_dot"], 23, pow(2, -57) * np.pi)
+    ans += data2bincomplement(ephemeris["M0"], 33, pow(2, -32) * np.pi)
     ans += data2bincomplement(ephemeris["e"], 33, pow(2, -34))
-    ans += data2bincomplement(ephemeris["omega"], 33, pow(2, -32))
+    ans += data2bincomplement(ephemeris["omega"], 33, pow(2, -32) * np.pi)
     return ans
 
 
@@ -46,9 +46,9 @@ def make_ephemeris2(ephemeris:dict) -> str:
         str: 二进制字符串形式的星历II
     """
     ans = ""
-    ans += data2bincomplement(ephemeris["Omega0"], 33, pow(2, -32))
-    ans += data2bincomplement(ephemeris["i0"], 33, pow(2, -32))
-    ans += data2bincomplement(ephemeris["Omega_dot"], 19, pow(2, -44))
+    ans += data2bincomplement(ephemeris["Omega0"], 33, pow(2, -32) * np.pi)
+    ans += data2bincomplement(ephemeris["i0"], 33, pow(2, -32) * np.pi)
+    ans += data2bincomplement(ephemeris["Omega_dot"], 19, pow(2, -44) * np.pi)
     ans += data2bincomplement(ephemeris["IDOT"], 15, pow(2, -44))
     ans += data2bincomplement(ephemeris["Cis"], 16, pow(2, -30))
     ans += data2bincomplement(ephemeris["Cic"], 16, pow(2, -30))

bdsTx/frame/b1i/d1/subframe2.py
@@ -8,6 +8,8 @@ FilePath: /bds-Sim/bdsTx/frame/b1i/d1/subframe2.py
 """
 
 
+import numpy as np
+
 from bdsTx.coding.b1i_bch import b1i_bch_encode_bin
 from bdsTx.frame.b1i.d1.pre import Pre
 from bdsTx.frame.util import data2bin, data2bincomplement
@@ -39,7 +41,9 @@ def create_subframe2(curr_time: float, eph: dict) -> str:
     # ----- Word  2 -----
     word2 = ""
     word2 += SOW[8:20]  # SOW last 12 bits
-    delta_n = data2bincomplement(eph["delta_n0"], 16, pow(2, -43))
+        # NOTE: In BDS frame, the unit of delta_n is pi/s, however in ephemeris file, the unit is rad/s
+        # SO we need to convert the unit. Some other parameters also need to be converted.
+    delta_n = data2bincomplement(eph["delta_n0"], 16, pow(2, -43) * np.pi)
     delta_n_h = delta_n[0:10]  # delta_n first 10 bits
     delta_n_l = delta_n[10:16]  # delta_n last 6 bits
     word2 += delta_n_h  # delta_n high 10 bits
@@ -57,7 +61,7 @@ def create_subframe2(curr_time: float, eph: dict) -> str:
     # ----- Word  4 -----
     word4 = ""
     word4 += cuc_l  # Cuc low 2 bits
-    M0 = data2bincomplement(eph["M0"], 32, pow(2, -31))
+    M0 = data2bincomplement(eph["M0"], 32, pow(2, -31) * np.pi)
     M0_h = M0[0:20]
     M0_l = M0[20:32]
     word4 += M0_h  # M0 high 20 bits

bdsTx/frame/b1i/d1/subframe3.py
@@ -1,11 +1,13 @@
 '''
 Author: symlpigeon
 Date: 2023-01-16 17:42:09
-LastEditTime: 2023-01-16 18:52:38
+LastEditTime: 2023-01-20 14:14:35
 LastEditors: symlpigeon
 Description: D1 导航电文子帧3
 FilePath: /bds-Sim/bdsTx/frame/b1i/d1/subframe3.py
 '''
+
+import numpy as np
 
 from bdsTx.coding.b1i_bch import b1i_bch_encode_bin
 from bdsTx.frame.b1i.d1.pre import Pre
@@ -38,7 +40,7 @@ def create_subframe3(curr_time: float, eph: dict) -> str:
     frame += b1i_bch_encode_bin(SOW_l + toe_m)  # toe middle 10 bits + SOW last 12 bits
     
     # ----- Word  3 -----
-    i_0 = data2bincomplement(eph["i0"], 32, pow(2, -31))
+    i_0 = data2bincomplement(eph["i0"], 32, pow(2, -31) * np.pi)
     i_0_h, i_0_l = i_0[0:17], i_0[17:32]
     frame += b1i_bch_encode_bin(toe_l + i_0_h)  # toe last 5 bits + i_0 high 17 bits
     
@@ -48,7 +50,7 @@ def create_subframe3(curr_time: float, eph: dict) -> str:
     frame += b1i_bch_encode_bin(i_0_l + cic_h)  # i_0 low 15 bits + Cic high 7 bits
     
     # ----- Word  5 -----
-    omega_dot = data2bincomplement(eph["Omega_dot"], 24, pow(2, -43))
+    omega_dot = data2bincomplement(eph["Omega_dot"], 24, pow(2, -43) * np.pi)
     omega_dot_h, omega_dot_l = omega_dot[0:11], omega_dot[11:24]
     frame += b1i_bch_encode_bin(cic_l + omega_dot_h)  # Cic low 11 bits + omega_dot high 11 bits
     
@@ -58,17 +60,17 @@ def create_subframe3(curr_time: float, eph: dict) -> str:
     frame += b1i_bch_encode_bin(omega_dot_l + cis_h)  # omega_dot low 13 bits + Cis high 9 bits
     
     # ----- Word  7 -----
-    idot = data2bincomplement(eph["IDOT"], 14, pow(2, -43))
+    idot = data2bincomplement(eph["IDOT"], 14, pow(2, -43) * np.pi)
     idot_h, idot_l = idot[0:13], idot[13:14]
     frame += b1i_bch_encode_bin(cis_l + idot_h)  # Cis low 9 bits + IDOT high 13 bits
     
     # ----- Word  8 -----
-    omega_0 = data2bincomplement(eph["Omega0"], 32, pow(2, -31))
+    omega_0 = data2bincomplement(eph["Omega0"], 32, pow(2, -31) * np.pi)
     omega_0_h, omega_0_l = omega_0[0:21], omega_0[21:32]
     frame += b1i_bch_encode_bin(idot_l + omega_0_h)  # IDOT low 1 bit + Omega_0 high 21 bits
     
     # ----- Word  9 -----
-    omega = data2bincomplement(eph["omega"], 32, pow(2, -31))
+    omega = data2bincomplement(eph["omega"], 32, pow(2, -31) * np.pi)
     omega_h, omega_l = omega[0:11], omega[11:32]
     frame += b1i_bch_encode_bin(omega_0_l + omega_h)  # Omega_0 low 11 bits + omega high 11 bits
     

bdsTx/frame/b1i/d1/subframe4.py
@@ -1,11 +1,13 @@
 '''
 Author: symlpigeon
 Date: 2023-01-19 10:14:09
-LastEditTime: 2023-01-19 11:40:12
+LastEditTime: 2023-01-20 14:19:54
 LastEditors: symlpigeon
 Description: D1 导航电文子帧4
 FilePath: /bds-Sim/bdsTx/frame/b1i/d1/subframe4.py
 '''
+
+from re import I
 
 import numpy as np
 
@@ -30,6 +32,7 @@ def create_subframe4(curr_time: float, alc: dict, page_id: int = 1) -> str:
     
     # Parameters
     WN, SOW = utc2bds(curr_time)
+    alc = alc[str(page_id)]
     sow = data2bin(SOW, 20, 1)
     sqrtA = data2bin(alc["SquareRootOfSemiMajorAxis"], 24, pow(2, -11))
         # NOTE: I'm not sure if multiply pi is a correct idea...
@@ -37,9 +40,9 @@ def create_subframe4(curr_time: float, alc: dict, page_id: int = 1) -> str:
         # So I wonder if i should multiply pi to the value...
         # Yes, the omega and m0 are also in the same situation.
     omega0 = data2bincomplement(alc["RightAscenAtWeek"], 24, pow(2, -23) * np.pi)
-    delta_i = data2bincomplement(alc["OrbitalInclination"], 16, pow(2, -19))
+    delta_i = data2bincomplement(alc["OrbitalInclination"], 16, pow(2, -19) * np.pi)
     toa = data2bin(alc["TimeOfApplicability"], 8, pow(2, 12))
-    omega_dot = data2bincomplement(alc["RateOfRightAscension"], 17, pow(2, -38))
+    omega_dot = data2bincomplement(alc["RateOfRightAscension"], 17, pow(2, -38) * np.pi)
     omega = data2bincomplement(alc["ArgumentOfPerigee"], 24, pow(2, -23) * np.pi)
     e = data2bin(alc["Eccentricity"], 17, pow(2, -21))
     m0 = data2bincomplement(alc["MeanAnomaly"], 24, pow(2, -23) * np.pi)

bdsTx/frame/b1i/d1/subframe5.py
@@ -1,7 +1,7 @@
 '''
 Author: symlpigeon
 Date: 2023-01-19 11:39:51
-LastEditTime: 2023-01-19 11:41:29
+LastEditTime: 2023-01-20 14:20:19
 LastEditors: symlpigeon
 Description: D1 导航电文子帧5
 FilePath: /bds-Sim/bdsTx/frame/b1i/d1/subframe5.py
@@ -32,16 +32,13 @@ def create_subframe5(curr_time: float, alc: dict, page_id: int = 1) -> str:
     
     # Parameters
     WN, SOW = utc2bds(curr_time)
+    alc = alc[str(page_id)]
     sow = data2bin(SOW, 20, 1)
     sqrtA = data2bin(alc["SquareRootOfSemiMajorAxis"], 24, pow(2, -11))
-        # NOTE: I'm not sure if multiply pi is a correct idea...
-        # The range of omega0 is [-1, 1], but the value in almanac file exceeds the range.
-        # So I wonder if i should multiply pi to the value...
-        # Yes, the omega and m0 are also in the same situation.
     omega0 = data2bincomplement(alc["RightAscenAtWeek"], 24, pow(2, -23) * np.pi)
-    delta_i = data2bincomplement(alc["OrbitalInclination"], 16, pow(2, -19))
+    delta_i = data2bincomplement(alc["OrbitalInclination"], 16, pow(2, -19) * np.pi)
     toa = data2bin(alc["TimeOfApplicability"], 8, pow(2, 12))
-    omega_dot = data2bincomplement(alc["RateOfRightAscension"], 17, pow(2, -38))
+    omega_dot = data2bincomplement(alc["RateOfRightAscension"], 17, pow(2, -38) * np.pi)
     omega = data2bincomplement(alc["ArgumentOfPerigee"], 24, pow(2, -23) * np.pi)
     e = data2bin(alc["Eccentricity"], 17, pow(2, -21))
     m0 = data2bincomplement(alc["MeanAnomaly"], 24, pow(2, -23) * np.pi)