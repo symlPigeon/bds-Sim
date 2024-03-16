e122c817963bac3466e2f42b8498324b858b1c38
symlpigeon <symlpigeon@foxmail.com>
2024-03-16 13:54:38 +0800

    Update readme and requirements.txt. Add a wrapper to generate prange and nav msg.

--- README.md
+++ README.md
@@ -14,3 +14,15 @@ BeiDou navigation satellite system signal simulator.
 在旧的项目中，我们将两部分代码合并在了一个仓库中，这既带来了配置上的麻烦，也使得整个项目变得混乱。
 
 因此我考虑将实际信号调制部分分离出去，建立一个新的分支，而这个分支仅仅包含了Python实现的导航电文和伪距生成部分。
+
+# 运行方法
+
+本项目需求应该是Python 3.10及以上，需求的第三方模块在`requirements.txt`中。
+
+运行`generate_beidou_info.py`可以生成导航电文和伪距信息：
+
+```bash
+python .\generate_beidou_info.py -p 120 30 0 -a .\bdsTx\satellite_info\almanac\tarc0190.23alc.json -e .\bdsTx\satellite_info\ephemeris\tarc0140.json -s B1I -i .\bdsTx\satellite_info\ionosphere\iono_corr.json -r .\bdsTx\coding\ranging_code\ -b 300 -o output.json
+```
+
+具体参数说明请查看`generate_beidou_info.py --help`的输出提示。
---
+++ generate_beidou_info.py
@@ -0,0 +1,77 @@
+import argparse
+import json
+import logging
+import time
+
+from bdsTx.handlers.satellite_handler import satelliteHandler
+from bdsTx.satellite_info.broadcast_type import SIGNAL_TYPE
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Generate Beidou Satellite Navigation Information."
+    )
+    parser.add_argument(
+        "-p",
+        "--pos",
+        type=float,
+        nargs=3,
+        help="Position to set, consisting of (longitude, latitude, height).",
+    )
+    parser.add_argument("-a", "--alc", type=str, help="Path to Almanac file.")
+    parser.add_argument("-e", "--eph", type=str, help="Path to Ephemeris file.")
+    parser.add_argument(
+        "-t", "--time", type=float, default=time.time(), help="Simulation start time."
+    )
+    parser.add_argument(
+        "-s",
+        "--signal_type",
+        type=str,
+        default="B1I",
+        help="Signal type, default is `B1I`.",
+    )
+    parser.add_argument(
+        "-i", "--iono_path", type=str, help="Path to ionosphere correction data."
+    )
+    parser.add_argument(
+        "-r", "--prn_path", type=str, help="Path to PRN file directory."
+    )
+    parser.add_argument("-b", "--broadcast_time", type=float, help="Broadcast time.")
+    parser.add_argument("-l", "--ldpc_path", type=str, help="Path to LDPC file.")
+    parser.add_argument("-o", "--output", type=str, help="Output file path.")
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    print(args)
+    logging.basicConfig(level=logging.INFO)
+    handler = satelliteHandler()
+    handler.set_alc_path(args.alc)
+    handler.set_eph_path(args.eph)
+    handler.set_time(args.time)
+    handler.set_position(args.pos)
+    handler.set_broadcast_time(args.broadcast_time)
+    handler.set_iono_path(args.iono_path)
+    handler.set_prn_path(args.prn_path)
+    match args.signal_type:
+        case "B1I":
+            handler.set_signal_type(SIGNAL_TYPE.B1I_SIGNAL)
+        case "B1C":
+            handler.set_signal_type(SIGNAL_TYPE.B1C_SIGNAL)
+    if args.ldpc_path:
+        handler.set_ldpc_path(args.ldpc_path)
+    handler.load_alc().load_eph().load_iono_corr().load_prn()
+    if args.ldpc_path:
+        handler.load_ldpc_mats()
+    handler.find_satellite()
+    handler.init_msg_gen()
+    msg = handler.generate()
+    logging.info("Done.")
+    with open(args.output, "w", encoding="utf-8") as f:
+        json.dump(msg, f, indent=4)
+
+
+if __name__ == "__main__":
+    main()
